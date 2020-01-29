1d06234611b725b9d5ea234e144b3f65d5aef3cf
siposm <sipos.miklos@nik.uni-obuda.hu>
2020-01-29 14:22:26 +0100

    added LA02 files

---
+++ LA-02-adatkotes/00-basic_databinding/basic_databinding/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace basic_databinding
+{
+    public class Person : INotifyPropertyChanged
+    {
+
+        //public string Name { get; set; }
+        //public string Age { get; set; }
+
+
+        #region interface-megvalositas
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Name"));
+            }
+        }
+
+        private string age;
+        public string Age
+        {
+            get { return age; }
+            set
+            {
+                age = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Age"));
+            }
+        }
+        #endregion
+
+    }
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Person MyPerson { get; set; }
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MyPerson = new Person();
+            MyPerson.Name = "Hi, I'm the Test Person.";
+            MyPerson.Age = "... and I'm 20 yo.";
+
+            this.DataContext = MyPerson;
+        }
+
+        private void UpdateClick(object sender, RoutedEventArgs e)
+        {
+            ;
+            // breakpoint >> MyPerson megtekintése >> látható, hogy felül lett írva a név (de ez mégsem jelenik meg a GUI-n...) >> inotify...
+        }
+    }
+}
---
+++ LA-02-adatkotes/01-StudentBook/StudentBook/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StudentBook
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            StudentListBox.Items.Add(new Student()
+            {
+                Name = "Kiss Lajos",
+                NeptunCode = "ROF982",
+                EnrollmentDate = new DateTime(2015, 09, 10),
+                IsActive = false
+            });
+
+            StudentListBox.Items.Add(new Student()
+            {
+                Name = "Tóth Imre",
+                NeptunCode = "BA7AF3",
+                EnrollmentDate = new DateTime(2012, 09, 10),
+                IsActive = true
+            });
+
+            StudentListBox.Items.Add(new Student()
+            {
+                Name = "Varga Andrea",
+                NeptunCode = "H3LL0C",
+                EnrollmentDate = new DateTime(2014, 09, 10),
+                IsActive = true
+            });
+        }
+
+        private void AddClick(object sender, RoutedEventArgs e)
+        {
+            StudentBindingWindow sbw = new StudentBindingWindow();
+            if (sbw.ShowDialog() == true)
+                StudentListBox.Items.Add(sbw.Student);
+        }
+
+        private void ModClick(object sender, RoutedEventArgs e)
+        {
+            if (StudentListBox.SelectedItem == null) return;
+            Student stud = StudentListBox.SelectedItem as Student;
+            StudentBindingWindow window = new StudentBindingWindow(stud);
+            if (window.ShowDialog() == true)
+            {
+                MessageBox.Show("OK, modified");
+            }
+            else
+            {
+                // todo: edit a copy => Prototype design pattern!
+                MessageBox.Show("BUG: cannot cancel a BINDING!!!");
+            }
+            StudentListBox.Items.Refresh(); // később observable collection...
+        }
+
+        private void DelClick(object sender, RoutedEventArgs e)
+        {
+            if (StudentListBox.SelectedItem == null) return;
+
+            StudentListBox.Items.Remove(StudentListBox.SelectedItem);
+        }
+
+        private void PurClick(object sender, RoutedEventArgs e)
+        {
+            StudentListBox.Items.Clear();
+        }
+    }
+}
