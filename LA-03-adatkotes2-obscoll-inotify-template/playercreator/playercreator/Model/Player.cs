dbd923a6e1803388399369edfb2efbe121aeec28
siposm <sipos.miklos@nik.uni-obuda.hu>
2020-01-31 23:24:17 +0100

    added project

---
+++ LA-03-adatkotes2-obscoll-inotify-template/playercreator/playercreator/Model/Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace playercreator.Model
+{
+    public class Player
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public int BirthYear { get; set; }
+        public bool IsActive { get; set; }
+        public int KillCount
+        {
+            get
+            {
+                int sum = 1;
+                for (int i = 0; i < 4; i++)
+                    sum *= int.Parse(BirthYear.ToString()[i].ToString()); // :)
+                return sum;
+            }
+            // set >> nincs set >> OneWay binding
+        }
+    }
+}
