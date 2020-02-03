736e8c8166fa57ff7d149c93a99981a2d8c1c174
siposm <sipos.miklos@nik.uni-obuda.hu>
2020-02-03 09:30:59 +0100

    added class file

---
+++ LA-04-command/folderlister/folderlister/BusinessLogic/DirectoryLogic.cs
@@ -0,0 +1,58 @@
+﻿using folderlister.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace folderlister.BusinessLogic
+{
+    class DirectoryLogic : IDirectoryLogic
+    {
+        public IEnumerable<DirectoryEntry> CollectDirectoryEntries(string currentDirectory)
+        {
+            var entries = new List<DirectoryEntry>();
+            if (currentDirectory == string.Empty)
+            {
+                foreach (DriveInfo akt in DriveInfo.GetDrives())
+                {
+                    entries.Add(new DirectoryEntry(akt.Name, true));
+                }
+            }
+            else
+            {
+                try
+                {
+                    foreach (string subDir in Directory.GetDirectories(currentDirectory))
+                    {
+                        entries.Add(new DirectoryEntry(subDir, true));
+                    }
+                    foreach (string file in Directory.GetFiles(currentDirectory))
+                    {
+                        entries.Add(new DirectoryEntry(file, false));
+                    }
+                }
+                catch (Exception)
+                {
+                }
+            }
+            return entries;
+        }
+
+        public void SelectEntry(DirectoryEntry entry)
+        {
+            if (entry == null) return;
+            if (entry.IsDir)
+            {
+                // Window dependency => extract to service call
+                MainWindow window = new MainWindow(entry.Name);
+                window.Show();
+            }
+            else
+            {
+                System.Diagnostics.Process.Start(entry.Name);
+            }
+        }
+    }
+}
---
+++ LA-04-command/folderlister/folderlister/BusinessLogic/IDirectoryLogic.cs
@@ -0,0 +1,16 @@
+﻿using folderlister.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace folderlister.BusinessLogic
+{
+    interface IDirectoryLogic
+    {
+        IEnumerable<DirectoryEntry> CollectDirectoryEntries(string currentDirectory);
+
+        void SelectEntry(DirectoryEntry entry);
+    }
+}
