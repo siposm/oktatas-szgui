da80ca7917e1de298b34ff1c1d61729c493eb7f4
siposm <sipos.miklos@nik.uni-obuda.hu>
2020-02-11 22:45:48 +0100

    added lookup stuff to routed demo

--- LA-01-alapok/3-routedEventDemo/routedDemo/routedDemo/MainWindow.xaml.cs
+++ LA-01-alapok/3-routedEventDemo/routedDemo/routedDemo/MainWindow.xaml.cs
@@ -20,15 +20,47 @@ namespace routedDemo
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string[] LookUpTable;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            LookUpTable = new string[]
+            {
+                // movie name ; year of release ; length (mins)
+                "Parasite;2007;186",
+                "Castle Rock;1990;109",
+                "Megszállottak viadala;1993;133",
+                "Into the Dark;1993;174",
+                "Tolvajok az erdőből;2001;119",
+                "Terror;2001;72",
+                "The Purge;2005;107",
+                "Black Lightning;1983;142",
+                "Cobra Kai;1993;183",
+                "Elit;1999;188",
+                "Aljas John;1982;73",
+                "Legacies: A sötétség öröksége;2014;150",
+                "Valós halál;2008;105",
+                "Sabrina hátborzongató kalandjai;2005;191",
+                "Lost in Space - Elveszve az űrben;2020;135",
+                "Diablero;1994;92"
+            };
         }
 
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            var x = (e.Source as Label).Content.ToString();
-            MessageBox.Show(x);
+            var name = (e.Source as Label).Content.ToString();
+
+            foreach (string item in LookUpTable)
+            {
+                if (item.Split(';')[0].Equals(name))
+                    MessageBox.Show(
+                        "NAME: " + name +
+                        "\nYEAR: " + item.Split(';')[1] +
+                        "\nLENGTH: " + item.Split(';')[2]
+                        );
+            }
         }
     }
 }
