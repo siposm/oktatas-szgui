3f35de311139039dfcef48fb7919736ae3988e78
siposm <sipos.miklos@nik.uni-obuda.hu>
2020-02-16 16:29:20 +0100

    added viewmodel version of studentbook
    not fully correct, but as a 'step' in the fully correct direction it is fine

---
+++ LA-02-adatkotes-alapok/2-StudentBook-version-ViewModel/studentBook_v2/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studentBook_v2
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string NeptunCode { get; set; }
+        public int? EnrollmentYear { get; set; } // add nullable option
+        public bool IsActive { get; set; }
+
+        // tostring nem ideális... >> datatemplate "teljes" adatkötéshez
+        public override string ToString()
+        {
+            string s = $"{Name} - [{NeptunCode}] : ";
+
+            if (IsActive)
+                s += "currently active";
+            else
+                s += "currently inactive";
+
+            return s;
+        }
+    }
+}
