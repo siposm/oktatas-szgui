eae2ca65d9664adfd1dc9df4243aada773c4283e
siposm <sipos.miklos@nik.uni-obuda.hu>
2020-02-24 18:34:50 +0100

    added basic example

---
+++ LA-03-adatkotes-halado/design_time_datacontext/design_time_datacontext/Model/Pizza.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace design_time_datacontext.Model
+{
+    class Pizza : INotifyPropertyChanged
+    {
+        //public int ID { get; set; }
+        //public string Name { get; set; }
+        //public int Diameter { get; set; }
+
+        private int id;
+        public int ID
+        {
+            get { return id; }
+            set { id = value; OnPropertyChanged(); }
+        }
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; OnPropertyChanged(); }
+        }
+
+        private int diameter;
+        public int Diameter
+        {
+            get { return diameter; }
+            set { diameter = value; OnPropertyChanged(); }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propname = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propname));
+        }
+    }
+}
---
+++ LA-03-adatkotes-halado/design_time_datacontext/design_time_datacontext/ViewModel/PizzaViewModel.cs
@@ -0,0 +1,24 @@
+﻿using design_time_datacontext.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace design_time_datacontext.ViewModel
+{
+    class PizzaViewModel
+    {
+        public ObservableCollection<Pizza> PizzaCollection { get; set; }
+
+        public PizzaViewModel()
+        {
+            PizzaCollection = new ObservableCollection<Pizza>();
+
+            PizzaCollection.Add(new Pizza() { ID = 1, Name = "Hungarian Hustle", Diameter = 45 });
+            PizzaCollection.Add(new Pizza() { ID = 2, Name = "Chili Cheese", Diameter = 20 });
+            PizzaCollection.Add(new Pizza() { ID = 3, Name = "Triple Z", Diameter = 105 });
+        }
+    }
+}
