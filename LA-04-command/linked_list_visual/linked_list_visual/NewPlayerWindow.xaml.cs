fd344dd83df3d7d46343394271014b1822e67ab6
siposm <sipos.miklos@nik.uni-obuda.hu>
2020-02-29 20:12:03 +0100

    new player window added

---
+++ LA-04-command/linked_list_visual/linked_list_visual/NewPlayerWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace linked_list_visual
+{
+    /// <summary>
+    /// Interaction logic for NewPlayerWindow.xaml
+    /// </summary>
+    public partial class NewPlayerWindow : Window
+    {
+        public NewPlayerWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+    }
+}
