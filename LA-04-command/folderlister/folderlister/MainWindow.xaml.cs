9a6965380badff02a2da6c104977b3fd7d439544
siposm <sipos.miklos@nik.uni-obuda.hu>
2020-03-02 11:19:59 +0100

    commend added

--- LA-04-command/folderlister/folderlister/MainWindow.xaml.cs
+++ LA-04-command/folderlister/folderlister/MainWindow.xaml.cs
@@ -38,9 +38,16 @@ namespace folderlister
             this.DataContext = viewModel;
         }
 
+
+
         // A minimálisan szükséges "logikán" kívül _nem_ teszünk mást a xaml.cs-be! 
         // Helyette command-okat használunk!
         // Lásd "open" button binding rész XAML-ben.
+        //
+        // A félévben jó és elég ha a button click-ekre vannak commandok létrehozva, másra nem kell.
+        // Akit érdekel viszont, hogy hogyan kell: https://docs.microsoft.com/en-us/archive/msdn-magazine/2013/may/mvvm-commands-relaycommands-and-eventtocommand
+        // Kulcsszavak: AttachedProperty, EventToCommand trigger
+
         private void ListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
             if (viewModel.SelectedEntry == null) return;
