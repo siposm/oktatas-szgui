802e822e3fa4453fa4990374ac6df8517c807b59
siposm <sipos.miklos@nik.uni-obuda.hu>
2020-03-06 11:05:52 +0100

    added login ervice

---
+++ LA-05-gyakorlas/neptun/neptun/BusinessLogic/LoginValidatorService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace neptun.BusinessLogic
+{
+    class LoginValidatorService
+    {
+        private Dictionary<string, string> GetValidUsers()
+        {
+            // ~ adatbázis lekérdezés
+
+            return new Dictionary<string, string>()
+            {
+                // username <> password
+                { "admin" , "admin123" },
+                { "hero" , "hero" },
+                { "root" , "toor" }
+            };
+        }
+
+        public bool ValidateCredentials(string username, string password)
+        {
+            Dictionary<string, string> database = GetValidUsers();
+            if (database.ContainsKey(username) && database.ContainsValue(password))
+                return true;
+
+            return false;
+        }
+    }
+}
