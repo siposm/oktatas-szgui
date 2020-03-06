7eb01208293ac0e42e3770f1882898d0e9edba2c
siposm <sipos.miklos@nik.uni-obuda.hu>
2020-03-06 11:20:58 +0100

    added init

--- LA-05-gyakorlas/neptun/neptun/ViewModel/MainWindowViewModel.cs
+++ LA-05-gyakorlas/neptun/neptun/ViewModel/MainWindowViewModel.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using neptun.BusinessLogic;
+using neptun.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace neptun.ViewModel
 {
     class MainWindowViewModel
     {
+        public ObservableCollection<Profile> ProfileCollection { get; set; }
+        public Profile SelectedProfile { get; set; }
+        public ICommand AddNewCommand { get; private set; }
+        public ICommand RemoveCommand { get; private set; }
+
+        public MainWindowViewModel()
+        {
+            ProfileCollection = new ObservableCollection<Profile>();
+
+            DataLoadingService dls = new DataLoadingService();
+            dls.FetchData().ForEach(x => ProfileCollection.Add(x));
+        }
     }
 }
