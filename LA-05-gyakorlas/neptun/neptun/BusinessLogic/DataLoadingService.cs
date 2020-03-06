eff8f1cb98c04a3382ea01238221d3486fdae4f8
siposm <sipos.miklos@nik.uni-obuda.hu>
2020-03-06 11:20:47 +0100

    added service

---
+++ LA-05-gyakorlas/neptun/neptun/BusinessLogic/DataLoadingService.cs
@@ -0,0 +1,22 @@
+﻿using neptun.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace neptun.BusinessLogic
+{
+    class DataLoadingService
+    {
+        public List<Profile> FetchData()
+        {
+            string url = "http://users.nik.uni-obuda.hu/siposm/db/students_v2.json";
+            WebClient wc = new WebClient();
+            string x = wc.DownloadString(url);
+            return JsonConvert.DeserializeObject<List<Profile>>(x);
+        }
+    }
+}
