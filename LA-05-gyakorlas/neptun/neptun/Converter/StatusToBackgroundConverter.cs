d510052a2d3beaf46445113cccd3c3c58621f1e0
siposm <sipos.miklos@nik.uni-obuda.hu>
2020-03-07 15:56:46 +0100

    status converter added (xaml + class)

---
+++ LA-05-gyakorlas/neptun/neptun/Converter/StatusToBackgroundConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace neptun.Converter
+{
+    class StatusToBackgroundConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isActive = (bool)value;
+            if (isActive)
+                return Brushes.Green; 
+            else
+                return Brushes.Red;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
