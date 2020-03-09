0f3355d33a3473bdd5b0528098cb0dcf25674f6e
siposm <sipos.miklos@nik.uni-obuda.hu>
2020-03-09 13:30:30 +0100

    added profile logic

---
+++ LA-05-komplex-feladat-gyakorlas/neptun/neptun/BusinessLogic/IProfileLogic.cs
@@ -0,0 +1,15 @@
+﻿using neptun.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace neptun.BusinessLogic
+{
+    interface IProfileLogic
+    {
+        void AddProfile(IList<Profile> collection, Profile toBeAdded);
+        void RemoveProfile(IList<Profile> collection, Profile toBeRemoved);
+    }
+}
---
+++ LA-05-komplex-feladat-gyakorlas/neptun/neptun/BusinessLogic/ProfileLogic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight.Messaging; // !!!
+using neptun.Model;
+
+namespace neptun.BusinessLogic
+{
+    class ProfileLogic : IProfileLogic
+    {
+        IMessenger messengerService;
+
+        public ProfileLogic(IMessenger service)
+        {
+            messengerService = service;
+        }
+
+        public void AddProfile(IList<Profile> collection, Profile toBeAdded)
+        {
+            // itt lehetne előtte sok féle/fajta vizsgálat, szűrés, stb. >> ezért van külön LOGIC-ba kiszervezve
+            collection.Add(toBeAdded);
+            messengerService.Send("ADD OK", "LogicResult");
+        }
+
+        public void RemoveProfile(IList<Profile> collection, Profile toBeRemoved)
+        {
+            collection.Remove(toBeRemoved);
+            messengerService.Send("DEL OK", "LogicResult");
+        }
+    }
+}
